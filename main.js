a9749053ea4379a6d4336938fd65073fddcd92a3
Tilegen <47211384+Bakitov-Tilegen@users.noreply.github.com>
2020-07-14 17:36:39 +0500

    создал новую ветку для 2 дз
    Новая ветка для дз 2

--- main.js
+++ main.js
@@ -1,18 +1,1 @@
-//1)
-// let temp = prompt('Температура с Цельсий на Фаренгейт')
-// const tempF = (9 / 5)* temp + 32
-// alert(tempF)
-
-//2)
-// let name = 'Vasiliy'
-// let admin = name
-// console.log(admin)
-
-//3) если число и строку при + то они склеится
-// let myString = '108'
-// let myNumber = 1000
-// alert(myString + myNumber)
-
-// 4) <script defer src="../js"></script> загр по очереди
-//    <script async src="../js"></script> загр если 2 js файла 1)200 строк 2)50 строк
-//    если даже 2 нижу по коду загр 2 потому что он не ждет загр 1)js файла как defer  
+// 2) ДЗ 
