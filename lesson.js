1bf2c3aa9e1bf1699a70bcfaf5ab2d5181a3b301
Tilegen <47211384+Bakitov-Tilegen@users.noreply.github.com>
2020-08-02 18:32:17 +0500

    5 lesson
    5 урок DOM

--- lesson.js
+++ lesson.js
@@ -1,168 +1,142 @@
-// const key = 'isMy'
+//==================================================>
+//1)
+// function numToObj (n) {
+// const obj = {}
 //
-// const human = {
-//   name:'Jony',
-//   walk:true,
-//   talk:true,
-//   ',':null, // можно задать вот такое имя
-//   [key]:true,
-// }
-// console.log(human)
-// // вызов
-// const human2 = Object.assign({}, human) // можно записать к пустому({empty}, nameOOP,2,3) ооп уже сущ
-// console.log(human2.name)
-// console.log(human2.isMy) === (human2['isMy']) // 2 вида вызова
-// console.log(human2[','])
-//
-// // add new element in OOP
-// human2.age = 22
-// console.log(human2.age)
-//
-// // delete element in OOP
-// delete human2.age
-// console.log(human2.age)
-//
-// for(const key in human2) {
-//
-// }
-// Object.keys(human2).filter(v => v === 'age')
-
-// let obj = {}
-//
-// function updateObj (object, key, value) {
-//   object[key] = value
-//   console.log(object)
-// }
-//
-// updateObj(obj, 'key', 'value0')
-//
-
-// let b = {
-//   status:'active' //01010101
-// }
-// function changeB(b) {
-//   b.status = 'diasbled'
-//   console.log(b)
-// }
-// changeB(b)
-// console.log(b)
-//
-
-// let computer = {
-//   name: 'Lenovo',
-//   cores: 8,
-//   power: false,
-//   start: function() {
-//     this.power = true
-//     console.log(`${this.name}: Power ON!`)
-//   },
-//   stop: function() {
-//     this.power = false
-//     console.log(`${this.name}: Power OFF!`)
-//   },
-// }
-// computer.stop()
-// const status = computer.power
-// console.log(status)
-
-//class js
-
-// class Computer {
-// name;
-// cores;
-// power;
-//
-// constructor (n) {
-//   this.name = n
-// }
-// start () {
-// this.power = true
-// console.log('Power on',this.power)
-// }
-// check () {
-//   console.log('!' in this)
+// if (n >= 0 && n <= 999) {
+//  const splitter = []
+//  for(let i = 1; i <= 3; i++){
+//    splitter.push(n % 10)
+//    n = Math.trunc(n / 10)
 //  }
+// [obj.ed, obj.ten, obj.hun] = splitter
 // }
+// return obj
+// }
+// let result = numToObj(123)
+// console.log(result)
+//==================================================>
+// let [firstName, , secondName] = 'Tilegen Adilbekovich Bakitov'.split(' ')
+
+// function numToObj(n) = {
+//  const obj = {}
+//   if (n >= 0 && n <= 999) {
+//     [obj.ed, obj.ten, obj.hun] = `${n}`.split('').reverse()//123-> ['1','2','3']->['3','2','1']
+//   }
+// }
+// numToObj(123)
+//==================================================>
+// const obj = {
+//   a:'qwer',
+//   b:[100,200,300],
+//   c:3,
+// }
+// let {a, b, c} = obj
+// console.log(a, b, c)
 //
-// const c1 = new Computer('Asus')
-// const c2 = new Computer('Apple')
-// const c3 = new Computer('HP')
-// const c4 = new Computer('Lenovo')
+// function soSomething (obj) {
+//   const {price, count} = obj
+// }
+
+//==================================================>
+// Диструктивное присвоение
+// let obj = {
+//   count:100,
+//   qwerty:200,
+//   color: ['red','gold','black','white',],
+//   storage:[32,64,128,256,],
+//   price:[499, 799, 1099,],
+//   // qwe:[{},{},{},],
+// }
+//   function doSomething () {
+//     let {color, storage, price} = obj
+//    // let color = obj.color //
+//     console.log(color[0], `${storage[0]}GB`, '$'+price[1] )
+//   }
+//   doSomething(obj)
+//==================================================>
+// DOCUMENT OBJECT MODULE
+
+// const el = document.getElementById('qwerty')
+// console.log(el)
+
+// const elClass = document.getElementsByClassName('link')
+// console.log(elClass)
+
+// const elQuery = document.querySelector('.link') // возвр Первый элемент
+// console.log(elQuery)
+
+// const elQAll = document.querySelectorAll('.link')
+// console.log(elQAll); // все элементы
+
+// const elName = document.querySelectorAll('[name=dont_know]')
+// console.log(elName);
+//==================================================>
+
+// const navSite = document.querySelector('.nav_site')
+// const navLink = navSite.querySelectorAll('.link')
+// console.log(navLink)
+//==================================================>
+
+// 1) Этот метод перезапишется будет только один li
+// const listContainer = document.querySelector('#list')
+// console.log(listContainer)
+// listContainer.innerHTML = '<li>New Element In Js</li>'
+
+// 2) Здесь же будет три li
+// const listContainer = document.querySelector('#list')
+// console.log(listContainer)
 //
-// console.log(c1)
-// console.log(c2)
-// console.log(c3)
-// console.log(c4)
+// const newListElement = document.createElement('li')
+// newListElement.innerHTML = 'New element JS'
 //
-// console.log(Computer)
+// listContainer.appendChild(newListElement)
+//==================================================>
+
+// const logo = document.querySelector('.logo')
+// // logo.classList.add('red') // Добав класса
+// //
+// // logo.classList.remove('red') // Удал класса
+// logo.classList.toggle('red') // переключатель если нету то доба если есть то убирает
 
 
-// class Animal {
-//   name = ''
-//   hieght = ''
-//   constructor (n) {
-//     // this.name = n
-//   }
-//
-//   breath () {
-//     console.log('breath!')
-//   }
-// }
-//
-// class Dog extends Animal { // extends наследовать
-// constructor(n) {
-//   super ()
-//   this.name = n
-//   // this.height = h
-// }
-//   bark () {
-//     console.log('Bark!')
-//   }
-// }
-//
-// const d = new Dog('Muhtar','80cm')
-// console.log(d)
-//
-// const d2 = new Dog('Rex','60cm')
-// console.log(d2)
-
-// Полимарфизм
-// class Animal {
-//   name = ''
-//   hieght = ''
-//   constructor (n) {
-//     // this.name = n
-//   }
-//
-//   breath () {
-//     console.log('breath!')
-//   }
-// voice () {
-//   console.log('?')
-//  }
-// }
-//
-// class Dog extends Animal { // extends наследовать
-// constructor(n) {
-//   super ()
-//   this.name = n
-//   // this.height = h
-// }
-//   voice() {
-//     console.log('Bark')
-//   }
-// }
-// class Cat extends Animal {
-//   constructor(n) {
-//     super(n)
-//   }
-//   voice() {
-//     console.log('Meow')
-//   }
-// }
-//
-//
-// const myPets = [new Cat(), new Dog()]
-// myPets.forEach(function(pet){
-//   pet.voice()
+// const btn = document.querySelector('.button')
+// btn.addEventListener('click', () => {
+//   btn.classList.toggle('red',)
+//   btn.style.height = '100px'
+//   // btn.style.display = 'none'
+//   // min-width ////css
+//   btn.style.minWidth = '200px' // camelCase
 // })
+
+//==================================================>
+// Системный Атрибуты в HTML обращение к ним
+//  const input = document.querySelector('input')
+//  const value = input.value
+//  console.log(value)
+//
+// const link = document.querySelector('.link_news')
+// console.log(link.href)
+//
+// // Можно уст и собственный Атрибуты пример
+// input.setAttribute('myattribute', true)
+// const newAttr = input.getAttribute('myattribute')
+//
+// // console.log(input.myattribute); //// не работает нужно передать через get
+// console.log(newAttr)
+
+//==================================================>
+// const menu = ['Home', 'About', 'Media', 'Links', 'News', 'Partner', 'NEW',]
+// const nav = document.querySelector('nav') // передал тег nav из html в js nav
+//
+// if(nav) { // условия если (nav) будет выпылнено
+//   menu.forEach(link => { //???
+//     const linkElement = document.createElement('a')// создал тег а и записал в linkElement
+//     linkElement.innerText = link // что же оно делает записывает ли оно значение который находитсЯ в массиве?
+//     linkElement.setAttribute('href',`/${link.toLowerCase()}/`) // атрибуту href как поставить например 1ссылке cart.html 2) gallery.html 3)news.html?
+//     linkElement.classList.add('link')// add class в тег
+//     console.log(linkElement) // вывод в консоль
+//     nav.appendChild(linkElement) // помести linkElement в nav
+//   })
+// }
+//==================================================>
